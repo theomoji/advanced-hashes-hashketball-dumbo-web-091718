d49f4a31f2f8c6c9f802fc0167af6f460b39bcb4
Shuo Wang <gken90@gmail.com>
2018-09-13 22:53:00 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,13 +4,17 @@ def game_hash
     :home{
       :team_name => " ",
       :colors => [],
-      :players
+      :players => {
+        
+      }
     }
 
     :away{
       :team_name => " ",
       :colors => [],
-      :players
+      :players => {
+
+      }
     }
   }
 
