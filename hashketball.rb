612aab8cfdd75fbc07b9650eea7ec8861f12c157
Shuo Wang <gken90@gmail.com>
2018-09-13 22:54:54 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -35,22 +35,4 @@ def game_hash
 end
 
 
-
-
-
-
-
-
-
-
-The :team_name key points to a string of the team name.
-The :colors key points to an array of strings that are that team's colors.
-The :players key points to a hash of players whose names (as strings) are the keys to a hash containing their stats. The values for each player's names and their stats can be found in the table below. The stats keys should be formatted like this:
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks
+puts game_hash
