6698592ff7bcb07dabd5d0653e5eb2058aff19ca
Shuo Wang <gken90@gmail.com>
2018-09-13 22:51:50 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,13 +3,13 @@ def game_hash
   {
     :home{
       :team_name => " ",
-      :colors
+      :colors => [],
       :players
     }
 
     :away{
       :team_name => " ",
-      :colors
+      :colors => [],
       :players
     }
   }
@@ -24,11 +24,7 @@ end
 
 
 
-The top level of the hash has two keys: :home, for the home team, and :away, for the away team.
-The values of the :home and :away keys are hashes. These hashes have the following keys:
-:team_name
-:colors
-:players
+
 The :team_name key points to a string of the team name.
 The :colors key points to an array of strings that are that team's colors.
 The :players key points to a hash of players whose names (as strings) are the keys to a hash containing their stats. The values for each player's names and their stats can be found in the table below. The stats keys should be formatted like this:
